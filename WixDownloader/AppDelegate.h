543986a2f01ff71abbebe3b36a2dfbb97f0d2e42
poofik <dmytro@MacBook-Pro.local>
2013-11-13 11:00:42 -0800

    A few improvements

--- WixDownloader/AppDelegate.h
+++ WixDownloader/AppDelegate.h
@@ -1,16 +1,16 @@
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSWindowController <NSWindowDelegate>
 {
     IBOutlet NSTextField* site;
     IBOutlet NSTextField* domain;
-    IBOutlet NSTextField* log;
     IBOutlet NSButton* download;
+    IBOutlet NSButton* media;
+    IBOutlet NSButton* php;
     IBOutlet NSProgressIndicator *loading;
-    IBOutlet NSButton *media;
+    IBOutlet NSProgressIndicator *progress;
+    IBOutlet NSTextField* percent;
 }
-
-@property (assign) IBOutlet NSWindow *window;
 
 - (IBAction) download_Click:(id)sender;
 - (void) Debug:(NSString*)d;
--- WixDownloader/AppDelegate.m
+++ WixDownloader/AppDelegate.m
@@ -4,6 +4,11 @@
 @implementation AppDelegate
 NSThread *thread;
 
+- (void)subWindowClosed:(NSNotification *)notification
+{
+    [NSApp terminate:self];
+}
+
 -(NSString*)downloadFile:(NSString*)url
 {
     [self Debug:[NSString stringWithFormat:@"Downloading URL: %@", url]];
@@ -39,6 +44,8 @@ NSThread *thread;
         [[NSFileManager defaultManager] createDirectoryAtPath:[NSString stringWithFormat:@"%@/media",DownloadPath] withIntermediateDirectories:YES attributes:nil error:&error];
     }
     
+    [progress setMaxValue:[jsonHTTP count]];
+    
     for(int i = 0; i < [jsonHTTP count]; i++)
     {
         if ([[jsonHTTP objectAtIndex:i] rangeOfString:@"\"http://"].location != NSNotFound)
@@ -56,7 +63,6 @@ NSThread *thread;
                     NSString* dirRoot= [[NSString alloc] init];
                     NSArray* domainRoot = [[[http objectAtIndex:h] stringByReplacingOccurrencesOfString:@"http://" withString:@""] componentsSeparatedByString: @"/"];
                     
-                    indexHTML = [indexHTML stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"http://%@",[domainRoot objectAtIndex:0]] withString:[domain stringValue]];
                     
                     //NSLog(@"%@", domainRoot);
                     
@@ -64,7 +70,7 @@ NSThread *thread;
                     {
                         dirRoot = [dirRoot stringByAppendingString:[NSString stringWithFormat:@"%@/",[domainRoot objectAtIndex:d]]];
                     }
-
+                    
                     dirRoot = [dirRoot stringByReplacingOccurrencesOfString:@"\"" withString:@""];
                     dirRoot = [dirRoot stringByReplacingOccurrencesOfString:@"]" withString:@""];
                     dirRoot = [dirRoot stringByReplacingOccurrencesOfString:@"}" withString:@""];
@@ -76,43 +82,90 @@ NSThread *thread;
                     NSString* webfile = [self downloadFile:[http objectAtIndex:h]];
                     if(webfile != NULL)
                     {
-                     
-                    if ([media state] == NSOnState)
-                    {
-                        NSArray *uri = [webfile componentsSeparatedByString: @","];
-                        for(int u = 0; u < [uri count]; u++)
+                        
+                        if ([media state] == NSOnState)
                         {
-                            if ([[uri objectAtIndex:u] rangeOfString:@"\"uri\":"].location != NSNotFound)
+                            NSArray *uri = [webfile componentsSeparatedByString: @","];
+                            for(int u = 0; u < [uri count]; u++)
                             {
-                                NSString* img = [[uri objectAtIndex:u] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
-                                img = [img stringByReplacingOccurrencesOfString:@"uri:" withString:@""];
-                                
-                                NSData* webBinary = [NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://static.wixstatic.com/media/%@",img]]];
-                                
-                                if ([webBinary writeToFile:[NSString stringWithFormat:@"%@/media/%@",DownloadPath,img] atomically:YES])
+                                if ([[uri objectAtIndex:u] rangeOfString:@"\"uri\":"].location != NSNotFound)
                                 {
-                                    [self Debug:[NSString stringWithFormat:@"Downloading Media: %@", img]];
+                                    NSString* img = [[uri objectAtIndex:u] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+                                    img = [img stringByReplacingOccurrencesOfString:@"uri:" withString:@""];
+                                    
+                                    NSData* webBinary = [NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://static.wixstatic.com/media/%@",img]]];
+                                    
+                                    if ([webBinary writeToFile:[NSString stringWithFormat:@"%@/media/%@",DownloadPath,img] atomically:YES])
+                                    {
+                                        [self Debug:[NSString stringWithFormat:@"Downloading Media: %@", img]];
+                                        
+                                        //TODO: wix has a dynamic image by size retreival, make php to emulate the same
+                                        if ([php state] == NSOnState)
+                                        {
+                                            
+                                        }
+                                    }
                                 }
                             }
                         }
+                        
+                        [webfile writeToFile:[NSString stringWithFormat:@"%@/%@/%@",DownloadPath,dirRoot,[domainRoot objectAtIndex:[domainRoot count]-1]] atomically:YES encoding:NSUTF8StringEncoding error:nil];
                     }
                     
-                    [webfile writeToFile:[NSString stringWithFormat:@"%@/%@/%@",DownloadPath,dirRoot,[domainRoot objectAtIndex:[domainRoot count]-1]] atomically:YES encoding:NSUTF8StringEncoding error:nil];
+                    BOOL replace = YES;
+                    
+                    if ([[jsonHTTP objectAtIndex:i] rangeOfString:@"\"emailServer\":"].location != NSNotFound)
+                    {
+                        replace = NO;
                     }
-              
+                    
+                    if ([php state] == NSOnState)  //TODO: create emulating email php?
+                    {
+                       
+                    }
+                    
+                    if ([media state] != NSOnState)
+                    {
+                        if ([[jsonHTTP objectAtIndex:i] rangeOfString:@"\"staticMediaUrl\":"].location != NSNotFound ||
+                            [[jsonHTTP objectAtIndex:i] rangeOfString:@"\"staticAudioUrl\":"].location != NSNotFound)
+                        {
+                            //NSLog(@">> %@",[jsonHTTP objectAtIndex:i]);
+                            replace = NO;
+                        }
+                    }
+                    
+                    if(replace == YES)
+                    {
+                         indexHTML = [indexHTML stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"http://%@/%@%@",[domainRoot objectAtIndex:0],dirRoot,[domainRoot objectAtIndex:[domainRoot count]-1]] withString:[NSString stringWithFormat:@"%@/%@%@",[domain stringValue],dirRoot,[domainRoot objectAtIndex:[domainRoot count]-1]]];
+                    }
+                    
                     break;
                 }
             }
         }
+        
+        float p = i / [jsonHTTP count] * 100;
+        //NSLog(@"Complete %ld %% (%d of %ld)", i/[jsonHTTP count], i,[jsonHTTP count]);
+        
+        [percent setStringValue:[NSString stringWithFormat:@"%.1f %%", p]];
+        [progress setDoubleValue:i];
     }
-
-    [indexHTML writeToFile:[NSString stringWithFormat:@"%@/index.html",DownloadPath] atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    
+    if ([php state] == NSOnState)
+    {
+        [indexHTML writeToFile:[NSString stringWithFormat:@"%@/index.php",DownloadPath] atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    }
+    else
+    {
+        [indexHTML writeToFile:[NSString stringWithFormat:@"%@/index.html",DownloadPath] atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    }
     
     [self Debug:@"Downloading Finished"];
     
     [download setTitle:@"Download"];
     [loading stopAnimation: self];
     [loading setHidden:TRUE];
+    [progress stopAnimation: self];
 }
 
 - (IBAction)download_Click:(id)sender;
@@ -125,29 +178,43 @@ NSThread *thread;
         
         [loading stopAnimation: self];
         [loading setHidden:TRUE];
+        [progress stopAnimation: self];
     }
     else
     {
-        [log setStringValue:@""];
         [loading setHidden:FALSE];
         [loading startAnimation: self];
         
+        [progress setDoubleValue:0];
+        [progress startAnimation: self];
+        
+        /*
+         if ([[site stringValue] rangeOfString:@"wix.com"].location == NSNotFound)
+         {
+         [site setStringValue:[NSString stringWithFormat:@"%@.wix.com",[site stringValue]]];
+         }*/
+        
+        if ([[site stringValue] rangeOfString:@"http://"].location == NSNotFound)
+        {
+            [site setStringValue:[NSString stringWithFormat:@"http://%@",[site stringValue]]];
+        }
+        
+        if ([[domain stringValue] rangeOfString:@"http://"].location == NSNotFound)
+        {
+            [domain setStringValue:[NSString stringWithFormat:@"http://%@",[domain stringValue]]];
+        }
+        
         thread = [[NSThread alloc] initWithTarget:self selector:@selector(startThread) object:nil];
         [thread start];
+        
         [self Debug:@"Downloading Started"];
         [download setTitle:@"Stop"];
     }
 }
 
-- (void)writeDebug:(NSString*)d
-{
-    [log setStringValue:[[log stringValue] stringByAppendingString:[NSString stringWithFormat:@"%@\n",d]]];
-}
-
 - (void)Debug:(NSString*)d
 {
-    //[self performSelectorInBackground:@selector(writeDebug:) withObject:d];
-    [log setStringValue:[[log stringValue] stringByAppendingString:[NSString stringWithFormat:@"%@\n",d]]];
+    //[log setStringValue:[[log stringValue] stringByAppendingString:[NSString stringWithFormat:@"%@\n",d]]];
     NSLog(@"%@",d);
 }
 @end
